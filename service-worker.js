1f07d078adcde06ddaba98f5f52f76daf6d076cd
Hezir Brasil <102698359+hezirbrasil@users.noreply.github.com>
2026-08-13 09:34:51 -0300

    Refresh app cache after removing login

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'workout-timer-v12';
+const CACHE_NAME = 'workout-timer-v13';
 const APP_SHELL = ['./','./index.html','./manifest.json','./icon.svg'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)).then(() => self.skipWaiting()));
