9c7a61c6c5506abfb66b8a8255aa13caa81bbad4
Hezir Brasil <102698359+hezirbrasil@users.noreply.github.com>
2026-08-12 23:34:49 -0300

    Force fresh app cache after bulk checkbox cleanup

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'workout-timer-v9';
+const CACHE_NAME = 'workout-timer-v10';
 const APP_SHELL = ['./','./index.html','./manifest.json','./icon.svg'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)).then(() => self.skipWaiting()));
